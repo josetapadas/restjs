41170c32ccfef5251a100e35c5d0eda67635635c
josetapadas <jose.tapadas@gmail.com>
2013-08-03 16:53:26 +0100

    created collection view and data is now presented on the webpage

--- todojs/public/static/todo.js
+++ todojs/public/static/todo.js
@@ -6,5 +6,29 @@ var TodoView = Backbone.View.extend({
 	render: function(){
 		var html = '<h3>' + this.model.get('name') + '</h3>';
 		$(this.el).html(html);
+		return this
+	}
+});
+
+// the todojs collection
+var TodoList = Backbone.Collection.extend({
+	model: TodoItem,
+	url: '/todojs'
+});
+
+var TodoListView = Backbone.View.extend({
+	initialize: function() {
+		this.collection.on('add', this.addItem, this);
+		this.collection.on('reset', this.addAllItems, this);
+	},
+	addItem: function(item){
+			var todoView = new TodoView({model:item});
+			this.$el.append(todoView.render().el);
+	},
+	addAllItems: function(){
+		this.collection.forEach(this.addItem, this);
+	},
+	render: function() {
+		this.addAllItems();	
 	}
 });
